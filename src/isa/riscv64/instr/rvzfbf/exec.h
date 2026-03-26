b9f537d14413e7b5256dd5b71a7af2fde096cd10
yu-yake2002 <yuyake02@outlook.com>
2026-03-26 15:20:15 +0800

    feat(bf16): Support risc-v BF16 extensions
    - Zfbfmin
    - Zvfbfmin
    - Zvfbfwma

---
+++ src/isa/riscv64/instr/rvzfbf/exec.h
@@ -0,0 +1,27 @@
+/***************************************************************************************
+* Copyright (c) 2014-2021 Zihao Yu, Nanjing University
+* Copyright (c) 2020-2022 Institute of Computing Technology, Chinese Academy of Sciences
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+#ifdef CONFIG_RV_ZFBF_MIN
+def_EHelper(fcvt_bf16_s) {
+  rtl_hostcall(s, HOSTCALL_FP, ddest, dsrc1, rz, FPCALL_CMD(FPCALL_F32ToBF16, FPCALL_W32));
+  rtl_fsr(s, ddest, ddest, FPCALL_W16);
+}
+
+def_EHelper(fcvt_s_bf16) {
+  rtl_hostcall(s, HOSTCALL_FP, ddest, dsrc1, rz, FPCALL_CMD(FPCALL_BF16ToF32, FPCALL_BF16));
+  rtl_fsr(s, ddest, ddest, FPCALL_W32);
+}
+#endif // CONFIG_RV_ZFBF_MIN
